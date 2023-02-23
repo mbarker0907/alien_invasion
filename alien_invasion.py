50b4ea8135fb95e8a1062ec3929dfbd290378aa0
mbarker0907 <mbarker0907@gmail.com>
2023-02-23 09:38:45 +0100

    Added event.ky K_Space to game_functions to allow ship to fire bullets.

--- alien_invasion.py
+++ alien_invasion.py
@@ -22,8 +22,8 @@ def run_game():
 	# Start the main loop for the game.
 	while True:
 		gf.check_events(ai_settings, screen, ship, bullets)
-		ship.update()
-		gf.update_screen(ai_settings, screen, ship)
+		ship.update()  
+		gf.update_screen(ai_settings, screen, ship, bullets)
 		bullets.update()
 		gf.update_screen(ai_settings, screen, ship, bullets)
 
