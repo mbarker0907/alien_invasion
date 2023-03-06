36d15111017ef5ce0deffaf1927814408bead56d
mbarker0907 <mbarker0907@gmail.com>
2023-03-06 16:02:49 +0100

    Created scoreboard and drew it on the screen.

--- game_functions.py
+++ game_functions.py
@@ -187,14 +187,17 @@ def check_events(ai_settings, stats, play_button, screen, ship, aliens, bullets)
 			mouse_x, mouse_y = pygame.mouse.get_pos()
 			check_play_button(ai_settings, screen, stats, play_button, ship, aliens, bullets, mouse_x, mouse_y)
 
-def update_screen(ai_settings, screen, stats, ship, aliens, bullets, play_button):
+def update_screen(ai_settings, screen, stats, sb, ship, aliens, bullets, play_button):
 	"""Update images on the screen and flip to the new screen."""
 	# Redraw the screen during each pass through the loop.
 	screen.fill(ai_settings.bg_color)
 	ship.blitme()
 	aliens.draw(screen)
 
-	#Draw the play button if the game is inactive.
+	# Draw the score information.
+	sb.show_score()
+
+	# Draw the play button if the game is inactive.
 	if not stats.game_active:
 		play_button.draw_button()
 
